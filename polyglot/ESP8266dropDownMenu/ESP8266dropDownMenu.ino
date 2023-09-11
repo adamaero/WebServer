04b35d4cfa55692d814c382b7e6604ff2da8054c
Adam <adamaero@hotmail.com>
2023-09-11 10:35:50 -0500

    Update ESP8266dropDownMenu.ino

--- polyglot/ESP8266dropDownMenu/ESP8266dropDownMenu.ino
+++ polyglot/ESP8266dropDownMenu/ESP8266dropDownMenu.ino
@@ -1,21 +1,21 @@
 /*
 
-Questions
+  Questions
 
-https://forum.arduino.cc/t/how-to-create-minimal-web-server-example-with-a-dropdown-menu/1167272
+  https://forum.arduino.cc/t/how-to-create-minimal-web-server-example-with-a-dropdown-menu/1167272
 
-https://arduino.stackexchange.com/questions/94323/how-to-create-minimal-web-server-example-with-a-dropdown-menu
-
-
+  https://arduino.stackexchange.com/questions/94323/how-to-create-minimal-web-server-example-with-a-dropdown-menu
 
 
 
 
-Based from 
-https://randomnerdtutorials.com/esp8266-web-server
 
-HTML tool
-https://www.w3schools.com/html/tryit.asp?filename=tryhtml_elem_select
+
+  Based from
+  https://randomnerdtutorials.com/esp8266-web-server
+
+  HTML tool
+  https://www.w3schools.com/html/tryit.asp?filename=tryhtml_elem_select
 */
 
 #include <ESP8266WiFi.h>
@@ -35,14 +35,14 @@ String header;
 //String output4State = "off";
 
 // Assign output variables to GPIO pins
-const int output5 = 14;    // D5 verizon  
+const int output5 = 14;    // D5 verizon
 const int output6 = 12;    // D6 tmobile
 const int output7 = 13;    // D7 atnt
 
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
@@ -51,7 +51,7 @@ void setup() {
   // Initialize the output variables as outputs
   pinMode(output5, OUTPUT);
   pinMode(output6, OUTPUT);
-  pinMode(output7, OUTPUT);  
+  pinMode(output7, OUTPUT);
   // Set outputs to LOW
   digitalWrite(output5, LOW);
   digitalWrite(output6, LOW);
@@ -83,7 +83,7 @@ void loop()
     currentTime = millis();
     previousTime = currentTime;
     while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
+      currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
@@ -100,86 +100,72 @@ void loop()
             client.println();
 
 
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) 
-            {
-              Serial.println("D5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            }
-            else if (header.indexOf("GET /5/off") >= 0) 
-            {
-              Serial.println("D5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            }
-            else if (header.indexOf("GET /6/on") >= 0) 
-            {
-              Serial.println("D6 on");
-              output4State = "on";
-              digitalWrite(output6, HIGH);
-            } 
-            else if (header.indexOf("GET /6/off") >= 0) 
-            {
-              Serial.println("D6 off");
-              output4State = "off";
-              digitalWrite(output6, LOW);
-            }
-            else if (header.indexOf("GET /7/on") >= 0) 
-            {
-              Serial.println("D7 on");
-              output4State = "on";
-              digitalWrite(output7, HIGH);
-            } 
-            else if (header.indexOf("GET /7/off") >= 0) 
-            {
-              Serial.println("D7 off");
-              output4State = "off";
-              digitalWrite(output7, LOW);
-            }
+
+
+
+
+
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
+            client.println("<title>Phone carrier selection</title>");
+
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
+            // CSS to style
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #77878A;}</style></head>");
-            '>Verizon</option>");
 
             // HERE
             // ↓
             // ↓
             // ↓
-            // ↓         
-              
+            // ↓
+
+            //const char page_before_provider[] = R"rawstr(<!DOCTYPE html>
+            //<p>Provider Selected: )rawstr";
+            //const char page_after_provider[] = "</p>\n</body>\n</html>\n";
+
+            // At the place where the page has to be sent.
+            client.print(page_before_provider);
+            if (provider == "verizon")
+              client.print("Verizon");
+            else if (provider == "tmobile")
+              client.print("T-Mobile");
+            else if (provider == "atnt")
+              client.print("AT&amp;T");
+            else
+              client.print(provider);
+            client.print(page_after_provider);
+
+
+
             // Web Page Heading
-            client.println("<form action='/action_page.php'>");
-            client.println("  <select id='providor' name='providor'>");
+            client.println("<form action='/?.php'>");
+            client.println("  <select id='carrier' name='carrier'>");
             client.println("    <option value='verizon'>Verizon</option>");
             client.println("    <option value='tmobile'>T-Mobile</option>");
-            client.println("    <option value='atnt'>AT&T</option>");
+            client.println("    <option value='atnt'>AT&amp;T</option>");
             client.println("  </select>");
             client.println("  <input type='submit'>");
             client.println("</form>");
-            
-            if (providor==verizon)
-              client.println("<p>Providor Selected: Verizon</p>");
-            else if (providor==tmobile)
-              client.println("<p>Providor Selected: T-Mobile"</p>");
-            else if (providor==atnt)
-              client.println("<p>Providor Selected: AT&T"</p>");    
-                
+
+            if (carrier == verizon)
+              client.println("<p>carrier Selected: Verizon</p>");
+            else if (carrier == tmobile)
+              client.println("<p>carrier Selected: T-Mobile" < / p > ");
+                             else if (carrier==atnt)
+                             client.println("<p>carrier Selected: AT & T"</p>");
+
             // ↑
             // ↑
             // ↑
             // ↑
-            
+
             client.println("</body></html>");
-            
+
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
